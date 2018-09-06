3a6b494d3e3756f323dd708d6a19a50a2e72ed74
Nathan Miranda <nate845@gmail.com>
2018-09-06 22:08:42 +0000

    automatically backed up by learn

---
+++ final-project/js/index.js
@@ -0,0 +1,39 @@
+/*
+Brainstorm
+*/
+
+/* HTML */
+/* 
+Header
+  - <h1>
+Tabs/Preset background
+  - <nav>, <h3>, <p>, <radio>
+Form,
+  - <form>, <input type="text">, <label>
+Display
+  - <div>
+    <h1 id="names"></h1>
+    <p>
+    <h3>
+    </div>
+*/
+
+/* CSS */
+/* display: flex/grid
+if grid, grid-template-columns: 1fr 1fr
+if flex in one container (width: 50%)
+if each side has its own flex, wrap it
+if float, float: left
+input selection changes text box to blue
+*/
+
+/* JS (event) */
+/* event? keydown? on every time */
+/* transition between backgrounds
+    - transition when clicking
+    - actually change the background
+  something like this: display.style.color = 'red'
+  onClick new image loads for background
+  also change the fonts when updating background
+  First Initials in the display slice() maybe?
+*/
